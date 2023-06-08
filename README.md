6b4dd5c734bca52c605d9acdf548859a902b12f5
Somnath Golui <code.somu@gmail.com>
2023-06-08 13:10:12 +0530

    Added next() function in the corsMiddleware

---
+++ README.md
@@ -0,0 +1,5 @@
+# Wellcome to Jeetcode.
+
+This is a leetcode clone.
+
+Run "cd client && npm run dev" and "cd server && npm start"
--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const corsMiddleware = (req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
     res.setHeader("Access-Control-Allow-Methods", "GET", "POST", "PUT", "DELETE");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type", "Authorization");
+    next();
 }
 
 const app = express();
